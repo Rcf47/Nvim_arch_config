6a595bf4b1e0577b49fe5d73db984b3cc6264a39
Rcf47 <xenos26@gmail.com>
2024-04-28 20:58:04 +0700

    feat: add shortcut H as beggining line

--- lua/vadim/remap.lua
+++ lua/vadim/remap.lua
@@ -266,3 +266,6 @@ vim.keymap.set("n", "<c-j>", "<c-w>j", { desc = "c-j move window down" })
 vim.keymap.set("n", "<c-h>", "<c-w>h", { desc = "c-h move window down" })
 vim.keymap.set("n", "<c-k>", "<c-w>k", { desc = "c-k move window down" })
 vim.keymap.set("n", "<c-l>", "<c-w>l", { desc = "c-l move window down" })
+
+-- H as ^ for beginning of line
+vim.keymap.set("n", "<s-h>", "^", { desc = "h as ^ for beginning of line" })
