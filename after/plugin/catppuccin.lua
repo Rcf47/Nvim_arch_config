687656d47e49b3ef5e3115294626b5dfa8f40600
Vadim Cherepanov <xenos26@gmail.com>
2024-05-27 18:21:02 +0700

    feat: add pmenu and pmenusel colors in catppucin theme

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -1,24 +1,27 @@
 local catppuccin = require("catppuccin")
 catppuccin.setup({
-	flavour = "mocha",
-	-- flavour = "macchiato",
-	-- flavour = "frappe",
-	-- flavour = "latte",
-	transparent_background = false,
-	show_end_of_buffer = false,
-	dim_inactive = {
-		enabled = false,
-		shade = "dark",
-		percentage = 0.20,
-	},
-	integrations = {
-		which_key = true,
-	},
-	color_overrides = {
-		mocha = {
-			base = "#10101a",
-		},
-	},
+  flavour = "mocha",
+  -- flavour = "macchiato",
+  -- flavour = "frappe",
+  -- flavour = "latte",
+  transparent_background = false,
+  show_end_of_buffer = false,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.20,
+  },
+  integrations = {
+    which_key = true,
+    cmp = true,
+    mason = true,
+    lsp_trouble = true,
+  },
+  color_overrides = {
+    mocha = {
+      base = "#10101a",
+    },
+  },
 })
 
 vim.cmd.colorscheme("catppuccin")
@@ -27,9 +30,10 @@ vim.cmd.colorscheme("catppuccin")
 
 local colors = require("catppuccin.palettes").get_palette()
 require("catppuccin.lib.highlighter").syntax({
-	-- MsgArea = { bg = colors.mantle },
-	NormalFloat = { bg = "none" },
-	CursorLine = { bg = colors.surface0 },
-	-- PmenuSel = { fg = colors.mantle, bg = colors.blue },
-	-- FloatBorder = { fg = colors.surface0, bg = colors.surface0 },
+  -- MsgArea = { bg = colors.mantle },
+  NormalFloat = { bg = colors.surface0 },
+  CursorLine = { bg = colors.surface0 },
+  PmenuSel = { fg = colors.mantle, bg = colors.blue },
+  Pmenu = { bg = colors.surface0 },
+  --FloatBorder = { fg = colors.surface0, bg = colors.surface0 },
 })
