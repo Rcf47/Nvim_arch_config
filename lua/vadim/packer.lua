4e220e2b4572169d637abc362cf56646be8917e3
Vadim Cherepanov <xenos26@gmail.com>
2024-06-15 22:14:42 +0700

    fix: fix bug with new version codeium vim
    Downgrade for 289eb724e5d6fab2263e94a1ad6e54afebefafb2 commit

--- lua/vadim/packer.lua
+++ lua/vadim/packer.lua
@@ -181,6 +181,11 @@ return require("packer").startup(function(use)
   --codeium
   use({
     "Exafunction/codeium.vim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
   })
 
   --better quickfix
