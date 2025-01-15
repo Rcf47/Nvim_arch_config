8f052a2060238aa4ed32278e1d171b9ef949700e
Vadim Cherepanov <xenos26@gmail.com>
2025-01-15 12:47:55 +0700

    feat: add arguments for live grep in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -20,6 +20,16 @@ end
 
 telescope.setup({
   defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+    },
     file_ignore_patterns = {
       "node_modules",
     },
